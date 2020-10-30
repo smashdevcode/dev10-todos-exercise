5b54a14334fbdac809b00482a61d2a6f54b1bb8a
James Churchill <james@smashdev.com>
2020-10-29 18:23:42 -0700

    Add updates

--- client/src/ToDos.js
+++ client/src/ToDos.js
@@ -5,8 +5,10 @@ class ToDos extends React.Component {
     super();
     this.state = {
       toDos: [],
+      id: 0,
       description: '',
       errors: [],
+      mode: 'Add',
     };
   }
 
@@ -30,7 +32,7 @@ class ToDos extends React.Component {
     });
   }
 
-  submitHandler = (event) => {
+  addSubmitHandler = (event) => {
     event.preventDefault();
 
     const {
@@ -63,6 +65,55 @@ class ToDos extends React.Component {
       });
   }
 
+  editSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const {
+      id,
+      description,
+    } = this.state;
+
+    fetch(`http://localhost:8080/api/todos/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id,
+        description,
+      }),
+    })
+      .then((response) => {
+        if (response.status === 204) {
+          this.setState({
+            id: 0,
+            description: '',
+            errors: [],
+            mode: 'Add',
+          });
+          this.getToDos();
+        } else if (response.status === 400) {
+          response.json().then((data) => this.setState({
+            errors: data,
+          }));
+        } else {
+          throw new Error(`Unexpected response: ${response}`);
+        }
+      });
+  }
+
+  editToDo = (toDoId) => {
+    fetch(`http://localhost:8080/api/todos/${toDoId}`)
+      .then((response) => response.json())
+      .then(({ id, description }) => {
+        this.setState({
+          id,
+          description,
+          mode: 'Edit',
+        });
+      });
+  }
+
   deleteToDo = (toDoId) => {
     fetch(`http://localhost:8080/api/todos/${toDoId}`, {
       method: 'DELETE',
@@ -81,26 +132,43 @@ class ToDos extends React.Component {
       toDos,
       description,
       errors,
+      mode,
     } = this.state;
 
     return (
       <>
         <h2>ToDos</h2>
-        <form onSubmit={this.submitHandler}>
-          {errors.length > 0 && (
-            <ul>
-              {errors.map((error) => (
-                <li key={error}>{error}</li>
-              ))}
-            </ul>
-          )}
-          <input value={description} onChange={this.changeHandler} type="text" />
-          <button type="submit">Add ToDo</button>
-        </form>
+        {mode === 'Add' && (
+          <form onSubmit={this.addSubmitHandler}>
+            {errors.length > 0 && (
+              <ul>
+                {errors.map((error) => (
+                  <li key={error}>{error}</li>
+                ))}
+              </ul>
+            )}
+            <input value={description} onChange={this.changeHandler} type="text" />
+            <button type="submit">Add ToDo</button>
+          </form>
+        )}
+        {mode === 'Edit' && (
+          <form onSubmit={this.editSubmitHandler}>
+            {errors.length > 0 && (
+              <ul>
+                {errors.map((error) => (
+                  <li key={error}>{error}</li>
+                ))}
+              </ul>
+            )}
+            <input value={description} onChange={this.changeHandler} type="text" />
+            <button type="submit">Update ToDo</button>
+          </form>
+        )}
         <ul>
           {toDos.map((toDo) => (
             <li key={toDo.id}>
               {toDo.description}
+              <button type="button" onClick={() => this.editToDo(toDo.id)}>Edit</button>
               <button type="button" onClick={() => this.deleteToDo(toDo.id)}>Delete</button>
             </li>
           ))}
