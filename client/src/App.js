75c80c1a0567d6f310def6cc845417ed40db40f0
James Churchill <james@smashdev.com>
2020-10-29 18:02:35 -0700

    Various updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import ToDos from './ToDos';
 
 function App() {
--- client/src/ToDos.js
+++ client/src/ToDos.js
@@ -5,75 +5,75 @@ class ToDos extends React.Component {
     super();
     this.state = {
       toDos: [],
-      description: ''
+      description: '',
     };
   }
 
   componentDidMount() {
+    this.getToDos();
+  }
+
+  getToDos = () => {
     fetch('http://localhost:8080/api/todos')
-        .then(response => response.json())
-        .then(data => {
-          this.setState({
-            toDos: data
-          });
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          toDos: data,
         });
+      });
   }
 
   changeHandler = (event) => {
     this.setState({
-      description: event.target.value
+      description: event.target.value,
     });
   }
 
   submitHandler = (event) => {
     event.preventDefault();
 
+    const {
+      description,
+    } = this.state;
+
     fetch('http://localhost:8080/api/todos', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        description: this.state.description
-      })
+        description,
+      }),
     })
-    .then(response => {
+      .then((response) => {
         if (response.status === 201) {
-            console.log('Success!');
-            response.json().then(data => console.log(data));
-
-            fetch('http://localhost:8080/api/todos')
-            .then(response => response.json())
-            .then(data => {
-              console.log(data);
-              this.setState({
-                toDos: data
-              });
-            });    
+          console.log('Success!');
+          response.json().then((data) => console.log(data));
+          this.getToDos();
         } else if (response.status === 400) {
-            console.log('Errors!');
-            response.json().then(data => console.log(data));
+          console.log('Errors!');
+          response.json().then((data) => console.log(data));
         } else {
-            console.log('Oops... not sure what happened here :(');
+          console.log('Oops... not sure what happened here :(');
         }
-    });
+      });
   }
 
-  // TODO Add submit event handler for the form
-  // TODO Use Fetch to make a POST
-  // TODO On success, use Fetch to get the list of ToDos
-
   render() {
+    const {
+      toDos,
+      description,
+    } = this.state;
+
     return (
       <>
         <h2>ToDos</h2>
-        {/* TODO Add form element with a single input element and button */}
         <form onSubmit={this.submitHandler}>
-          <input value={this.state.description} onChange={this.changeHandler} type="text" />
+          <input value={description} onChange={this.changeHandler} type="text" />
           <button type="submit">Add ToDo</button>
         </form>
         <ul>
-          {this.state.toDos.map(toDo => (
+          {toDos.map((toDo) => (
             <li key={toDo.id}>{toDo.description}</li>
           ))}
         </ul>
