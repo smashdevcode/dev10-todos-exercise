097c25e56202c0ab608faba413c0115d11a9f321
James Churchill <james@smashdev.com>
2020-10-30 10:00:53 -0700

    Add support for update

--- client/src/ToDos.js
+++ client/src/ToDos.js
@@ -5,6 +5,7 @@ class ToDos extends React.Component {
     super();
     this.state = {
       toDos: [],
+      id: 0,
       description: ''
     };
   }
@@ -14,7 +15,9 @@ class ToDos extends React.Component {
         .then(response => response.json())
         .then(data => {
           this.setState({
-            toDos: data
+            toDos: data,
+            id: 0,
+            description: ''
           });
         });
   }
@@ -29,7 +32,7 @@ class ToDos extends React.Component {
     });
   }
 
-  submitHandler = (event) => {
+  addSubmitHandler = (event) => {
     event.preventDefault();
 
     fetch('http://localhost:8080/api/todos', {
@@ -72,18 +75,93 @@ class ToDos extends React.Component {
     });
   }
 
+  // DONE add "edit" button
+  // DONE stub out the handler method
+  // DONE fetch the todo and update state
+  // DONE add the form
+  // DONE add the form submit handler
+  // DONE reset the state
+
+  // TODO use "mode" to control form visibility
+  // TODO display errors
+
+  editToDoHandler = (toDoId) => {
+    console.log('Edit todo... ' + toDoId);
+
+    // GET http://localhost:8080/api/todos/1 HTTP/1.1
+    fetch(`http://localhost:8080/api/todos/${toDoId}`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          id: data.id,
+          description: data.description
+        });
+      });
+      // you can optionally use destructuring...
+      // .then(({ id, description }) => {
+      //   this.setState({
+      //     id,
+      //     description
+      //   });
+      // });
+  }
+
+  editSubmitHandler = (event) => {
+    event.preventDefault();
+
+    // const id = this.state.id;
+    // const description = this.state.description;
+
+    const { id, description } = this.state;
+
+    // PUT http://localhost:8080/api/todos/1 HTTP/1.1
+    // Content-Type: application/json
+    
+    // {
+    //     "id": 1,
+    //     "description": "Updated todo."
+    // }
+    
+    fetch(`http://localhost:8080/api/todos/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        id,
+        description
+      })
+    })
+    .then(response => {
+        if (response.status === 204) {
+            console.log('Success!');
+            this.getToDos();
+        } else if (response.status === 400) {
+            console.log('Errors!');
+            response.json().then(data => console.log(data));
+        } else {
+            console.log('Oops... not sure what happened here :(');
+        }
+    });
+  }
+
   render() {
     return (
       <>
         <h2>ToDos</h2>
-        <form onSubmit={this.submitHandler}>
+        <form onSubmit={this.addSubmitHandler}>
           <input value={this.state.description} onChange={this.changeHandler} type="text" />
           <button type="submit">Add ToDo</button>
+        </form>
+        <form onSubmit={this.editSubmitHandler}>
+          <input value={this.state.description} onChange={this.changeHandler} type="text" />
+          <button type="submit">Update ToDo</button>
         </form>
         <ul>
           {this.state.toDos.map(toDo => (
             <li key={toDo.id}>
               {toDo.description}
+              <button type="button" onClick={() => this.editToDoHandler(toDo.id)}>Edit</button>
               <button type="button" onClick={() => this.deleteToDoHandler(toDo.id)}>Delete</button>
             </li>
           ))}
