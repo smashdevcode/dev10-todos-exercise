c892f3c69edbcb7eb570269cca986eb8603cd6e4
James Churchill <james@smashdev.com>
2020-11-12 08:31:03 -0800

    Add token-based security

---
+++ server/src/main/java/learn/todos/controllers/AuthController.java
@@ -0,0 +1,62 @@
+package learn.todos.controllers;
+
+import learn.todos.security.JwtConverter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class AuthController {
+    // The `AuthenticationManager` interface defines a single method `authenticate()`
+    // that processes an Authentication request.
+    private final AuthenticationManager authenticationManager;
+    private final JwtConverter converter;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtConverter converter) {
+        this.authenticationManager = authenticationManager;
+        this.converter = converter;
+    }
+
+    @PostMapping("/api/authenticate")
+    public ResponseEntity<?> authenticate(@RequestBody Map<String, String> credentials) {
+        // The `UsernamePasswordAuthenticationToken` class is an `Authentication` implementation
+        // that is designed for simple presentation of a username and password.
+        UsernamePasswordAuthenticationToken authToken =
+                new UsernamePasswordAuthenticationToken(credentials.get("username"), credentials.get("password"));
+
+        try {
+            // The `Authentication` interface Represents the token for an authentication request
+            // or for an authenticated principal once the request has been processed by the //
+            // `AuthenticationManager.authenticate(Authentication)` method.
+            Authentication authentication = authenticationManager.authenticate(authToken);
+
+            if (authentication.isAuthenticated()) {
+                // The `User` class models core user information retrieved by a `UserDetailsService`.
+                // Developers may use this class directly, subclass it, or write their own
+                // `UserDetails` implementation from scratch.
+                User user = (User)authentication.getPrincipal();
+
+                String jwtToken = converter.getTokenFromUser(user);
+
+                HashMap<String, String> map = new HashMap<>();
+                map.put("jwt_token", jwtToken);
+
+                return new ResponseEntity<>(map, HttpStatus.OK);
+            }
+        } catch (AuthenticationException ex) {
+            System.out.println(ex);
+        }
+
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+    }
+}
---
+++ server/src/main/java/learn/todos/security/SecurityConfig.java
@@ -0,0 +1,70 @@
+package learn.todos.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final JwtConverter converter;
+
+    public SecurityConfig(JwtConverter converter) {
+        this.converter = converter;
+    }
+
+    // This method allows configuring web based security for specific http requests.
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.authorizeRequests()
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/todos").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/todos/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/todos").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/todos/*").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/todos/*").hasRole("ADMIN")
+                .and()
+                .addFilter(new JwtRequestFilter(authenticationManager(), converter))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        var userBuilder = User.withUsername("user")
+                .password("user").passwordEncoder(password -> encoder.encode(password))
+                .roles("USER");
+
+        var adminBuilder = User.withUsername("admin")
+                .password("admin").passwordEncoder(password -> encoder.encode(password))
+                .roles("ADMIN");
+
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder)
+                .withUser(adminBuilder);
+    }
+
+    @Bean
+    public PasswordEncoder getEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
